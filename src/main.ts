c23cf50c79f6002d39a93921ca4302f72a87341c
zhangjun <1548293573@qq.com>
2021-03-30 15:04:34 +0800

    add 部署arcgis api

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import esriConfig  from '@arcgis/core/config.js';
+esriConfig.assetsPath = './assets';
 
 Vue.config.productionTip = false;
 
---
+++ src/views/mapIndex.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="viewDiv"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Map from '@arcgis/core/Map';
+import MapView from '@arcgis/core/views/MapView';
+
+@Component({ name: 'MapIndex' })
+export default class MapIndex extends Vue {
+  view: MapView | undefined;
+
+  mounted() {
+    const map = new Map({
+      basemap: 'satellite',
+    });
+
+    this.view = new MapView({
+      container: 'viewDiv',
+      map,
+      zoom: 4,
+      center: [15, 65], // longitude, latitude
+    });
+  }
+}
+</script>
+
+<style scoped lang="less">
+#viewDiv {
+  padding: 0;
+  margin: 0;
+  height: 100%;
+  width: 100%;
+}
+</style>
